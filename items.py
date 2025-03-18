ee0785f4cf61e367c6ffae95be16e32737664c58
Kiia Mäntylä <kiia.mantyla@gmail.com>
2025-03-18 15:02:25 +0200

    Rename items.py to movies.py

--- items.py
+++
@@ -1,7 +0,0 @@
-import db
-
-def add_item(title, year, recommendation, user_id):
-    sql = """INSERT INTO movies (title, year, recommendation, user_id)
-             VALUES (?, ?, ?, ?)"""
-
-    db.execute(sql, [title, year, recommendation, user_id])
