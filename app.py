354e021d8f9ebf3669d465c041d2acf64805875b
Kiia Mäntylä <kiia.mantyla@gmail.com>
2025-03-18 11:21:56 +0200

    Add new movie form

--- app.py
+++ app.py
@@ -15,9 +15,30 @@ def index():
     return render_template("index.html")
 
 
+@app.route("/new_item")
+def new_item():
+    return render_template("new_item.html")
+
+
+@app.route("/create_item", methods=["POST"])
+def create_item():
+    title = request.form["title"]
+    year = request.form["year"]
+    recommendation = request.form["recommendation"]
+    user_id = session["user_id"]
+
+    sql = """INSERT INTO movies (title, year, recommendation, user_id)
+             VALUES (?, ?, ?, ?)"""
+
+    db.execute(sql, [title, year, recommendation, user_id])
+
+    return redirect("/")
+
+
 @app.route("/register")
 def register():
     return render_template("register.html")
+
 
 @app.route("/create", methods=["POST"])
 def create():
@@ -46,10 +67,13 @@ def login():
         username = request.form["username"]
         password = request.form["password"]
 
-        sql = "SELECT password_hash FROM users WHERE username = ?"
-        password_hash = db.query(sql, [username])[0][0]
+        sql = "SELECT id, password_hash FROM users WHERE username = ?"
+        result = db.query(sql, [username])[0]
+        user_id = result["id"]
+        password_hash = result["password_hash"]
 
         if check_password_hash(password_hash, password):
+            session["user_id"] = user_id
             session["username"] = username
             return redirect("/")
         else:
@@ -57,5 +81,6 @@ def login():
 
 @app.route("/logout")
 def logout():
+    del session["user_id"]
     del session["username"]
     return redirect("/")
--- schema.sql
+++ schema.sql
@@ -3,3 +3,11 @@ CREATE TABLE users (
     username TEXT UNIQUE,
     password_hash TEXT
 );
+
+CREATE TABLE movies (
+    id INTEGER PRIMARY KEY,
+    title TEXT,
+    year TEXT,
+    recommendation TEXT,
+    user_id INTEGER REFERENCES users
+);
