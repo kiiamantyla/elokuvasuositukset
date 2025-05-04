a40a7f2ea036f8e532cf38dd279f572562796953
Kiia Mäntylä <kiia.mantyla@gmail.com>
2025-05-04 19:09:41 +0300

    Add genres

--- init.sql
+++ init.sql
@@ -5,6 +5,19 @@ INSERT INTO classes (title, value) VALUES ('genre', 'draama');
 INSERT INTO classes (title, value) VALUES ('genre', 'kauhu');
 INSERT INTO classes (title, value) VALUES ('genre', 'fantasia');
 INSERT INTO classes (title, value) VALUES ('genre', 'toiminta');
+INSERT INTO classes (title, value) VALUES ('genre', 'scifi');
+INSERT INTO classes (title, value) VALUES ('genre', 'rakkaus');
+INSERT INTO classes (title, value) VALUES ('genre', 'jännitys');
+INSERT INTO classes (title, value) VALUES ('genre', 'seikkailu');
+INSERT INTO classes (title, value) VALUES ('genre', 'rikos');
+INSERT INTO classes (title, value) VALUES ('genre', 'dokumentti');
+INSERT INTO classes (title, value) VALUES ('genre', 'lännenelokuva');
+INSERT INTO classes (title, value) VALUES ('genre', 'musikaali');
+INSERT INTO classes (title, value) VALUES ('genre', 'animaatio');
+INSERT INTO classes (title, value) VALUES ('genre', 'lyhytelokuva');
+INSERT INTO classes (title, value) VALUES ('genre', 'sotaelokuva');
+INSERT INTO classes (title, value) VALUES ('genre', 'anime');
+
 
 INSERT INTO classes (title, value) VALUES ('ikäraja', 'S');
 INSERT INTO classes (title, value) VALUES ('ikäraja', 'K7');
